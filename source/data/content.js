275bf97257a360509cef48828045b39fdfbdb540
Andrew Groff <andrew.p.groff@gmail.com>
2014-04-25 13:51:35 +0300

    Initial commit. Currently unstable on chrome, production version on firefox

---
+++ source/data/content.js
@@ -0,0 +1,469 @@
+
+/**
+ * Originally based on the userscript "Reddcoin QuickTip" by Jacce
+ * http://userscripts.org/scripts/show/406551?3
+ *
+ * Ported to a chrome plugin by:
+ * Andy Groff (andy@groff.co)
+ * http://groff.co
+ *
+ */
+
+function dbg(variable)
+{
+    console.log(variable);
+}
+
+dbg("starting...");
+
+var RDD = {
+
+    vars : {
+        modalWidth: 450
+    },
+
+    tipList: [
+        50,
+        100,
+        200,
+        500,
+
+        1000,
+        2000,
+        5000,
+        10000,
+
+        25000,
+        50000,
+        100000,
+        500000
+    ],
+
+    currentTextArea : false
+}
+
+
+RDD.modal = (function(){
+    var pri = {
+            vars: {
+                callback    : false,
+                overlay     : false,
+                popup       : false,
+                requireUser : false
+            }
+        },
+
+        pub = {};
+
+    pri.doTip = function(){
+        var tipAmount = $("#reddTipAmount").val(),
+            tipUser = $.trim($("#reddTipUser").val()),
+            float = parseFloat(tipAmount);
+
+        if(isNaN(float)){
+            $("#reddTipAmount").addClass("error");
+            return;
+        }
+        else {
+            $("#reddTipAmount").removeClass("error");
+        }
+
+        if(pri.vars.requireUser && tipUser === ""){
+            $("#reddTipUser").addClass("error");
+            return;
+        }
+        else {
+            $("#reddTipUser").removeClass("error");
+        }
+
+        //ensure @ sign
+        if(tipUser.length > 0){
+            if(tipUser.charAt(0) !== '@') {
+                tipUser = '@'+tipUser;
+            }
+        }
+
+        pri.vars.callback(tipAmount, tipUser);
+        RDD.modal.close();
+    }
+
+    pri.buildModalHeader = function(){
+        var headerSrc = RDD.helpers.url('/reddcoin_header_logo.png'),
+            imgHtml = '<img id="reddCoinPopupImage" src="'+headerSrc+'">';
+
+        $("#reddCoinPopupHeader").html(imgHtml);
+    };
+
+    pri.bindMainButtons = function(){
+
+        $("#reddTipCancel").click(pub.close);
+
+        $("#reddTipButton").click(function(){
+            pri.doTip();
+        });
+    };
+
+    pri.addQuickButtons = function(){
+        var tipHtml = '';
+
+        $.each(RDD.tipList, function(i, tipAmount){
+
+            var a = ''
+                  + '<a href="javascript:void(0);" class="rddQuickTip" data-tipValue="' + tipAmount + '">'
+                  + tipAmount.toLocaleString()
+                  + '</a>';
+
+            tipHtml += a
+        });
+
+        $("#reddCoinTipContainer").html(tipHtml);
+
+        $(".rddQuickTip").click(function(){
+            var val = $(this).attr("data-tipValue");
+            $("#reddTipAmount").val(val);
+            pri.doTip();
+        });
+    };
+
+    pub.open = function(callback, showUser, requireUser){
+        var $userInput = $("#reddTipUser");
+
+        pri.vars.callback = callback;
+        pri.vars.requireUser = requireUser || false;
+
+        if(showUser){
+            $userInput.show();
+        }
+        else {
+            $userInput.hide();
+        }
+
+        pri.vars.overlay.fadeIn('fast');
+        pri.vars.popup.fadeIn('slow', function(){
+            //if the current site hooks the dialog open event, call it.
+            if(RDD.site.hookTipOpen != undefined) {
+                RDD.site.hookTipOpen();
+            }
+        });
+        pri.vars.popup.css('top', '60px');
+    };
+
+    pub.close = function(){
+        pri.vars.overlay.fadeOut('slow');
+        pri.vars.popup.fadeOut('fast');
+        $("#reddTipAmount").val("");
+    };
+
+    pub.initialize = function(){
+
+        dbg("inside initialize modal!!!!");
+
+        RDD.helpers.getPopupHtml(function(html){
+            dbg("popup loaded: " + html);
+            var win = {
+                height : $(window).height(),
+                width  : $(window).width()
+            };
+
+            $(html).appendTo("body");
+
+            //store the jquery objects we'll need frequently
+            pri.vars.overlay = $("#reddCoinPopupContainer");
+            pri.vars.popup   = $("#reddCoinPopup");
+
+            //make the overlay take up the entire window
+            pri.vars.overlay.height(win.height);
+
+            pri.vars.popup.width(RDD.vars.modalWidth);
+            pri.vars.popup.offset({
+                top: 50,
+                left: (win.width - RDD.vars.modalWidth) / 2
+            });
+
+            pri.buildModalHeader();
+            pri.addQuickButtons();
+            pri.bindMainButtons();
+        });
+        dbg("initialize finished");
+    }
+
+    //return the public object to expose global functions.
+    return pub;
+})()
+
+RDD.helpers = {
+
+    isChrome: function(){
+        dbg("checking chrome ----------------");
+        try {
+            if(chrome) {
+                return true;
+            }
+        } catch(e) {}
+
+        dbg("NOT   chrome !!!!!!!");
+        return false;
+    },
+
+    getPopupHtml: function(callback){
+        dbg(self.options);
+
+        if(RDD.helpers.isChrome()){
+            $.get(RDD.helpers.url('/popup.html'), function(html){
+                callback(html);
+            });
+            return;
+        }
+
+        callback(self.options.popupHtml)
+        dbg(self.options.popupHtml)
+    },
+
+    url: function(path){
+
+        if(RDD.helpers.isChrome()){
+            return chrome.extension.getURL(path);
+        }
+
+        var str = self.options.baseUrl;
+        str = str.substring(0, str.length - 1)
+        return str + path;
+
+//        return require("sdk/self").data.url(path);
+//
+    },
+
+    getCommand: function(command, amount, user){
+        command = command.replace('{AMOUNT}', amount);
+
+        if(user !== undefined){
+            command = command.replace('{RECIPIENT}', user);
+        }
+
+        return command;
+    },
+
+    getCurrentSite: function(){
+        var domain = document.domain;
+
+        switch (domain){
+            case 'www.twitter.com':
+            case 'twitter.com':
+                return 'twitter';
+
+            case 'www.reddit.com':
+            case 'reddit.com':
+                return 'reddit';
+
+            case 'www.twitch.tv':
+            case 'twitch.tv':
+                return 'twitch';
+        }
+
+        return false;
+    }
+}
+
+/**
+ * This will hold the current site.
+ *
+ */
+RDD.site = {};
+
+/**
+ * This holds all the available sites.
+ *
+ */
+RDD.sites = {};
+
+RDD.sites.reddit = {
+
+    buttonHtml : '<button class="tip" type="button">tip</button>',
+    command    : '+/u/reddtipbot {AMOUNT} RDD',
+
+    tipClicked : function(){
+
+        // bind the show ui function to button click
+        var input = $(this).closest('.usertext-edit').find('textarea');
+
+        RDD.modal.open(function(tipAmount){
+            var value = input.val() + '\n\n';
+
+            value += RDD.helpers.getCommand(RDD.site.command, tipAmount)
+
+            input.val(value);
+        });
+    },
+
+    addButtons : function(){
+        // Add buttons for all areas where there should be one
+        $('.usertext-buttons').each(function(){
+            var $tipButton;
+
+            //make sure there's not already a tip button here.
+            if ($(this).find('.tip').length == 0)
+            {
+                //create button
+                $tipButton = $(RDD.site.buttonHtml);
+
+                //append the tip button
+                $(this).find('.cancel').after($tipButton);
+            }
+        });
+    },
+
+    initialize: function(){
+
+        //add initial buttons
+        RDD.site.addButtons();
+
+        // bind clicks on reply to also add a button
+        $('.noncollapsed > .flat-list > li').each(function(){
+            var text = $.trim($(this).text()).toLowerCase();
+
+            if(text === 'reply') {
+                $(this).click(RDD.site.addButtons);
+            }
+        });
+
+        //bind to body click, filter for .tip buttons
+        $("body").on("click", ".tip", RDD.site.tipClicked);
+    }
+};
+
+
+
+RDD.sites.twitter = {
+
+    buttonHtml : '<button class="btn primary-btn tip-action tip-btn js-tip-btn" type="button">\
+	<span class="button-text tip-text">\
+		Tip\
+	</span>\
+</button>',
+    command    : ' @tipreddcoin +tip {RECIPIENT} {AMOUNT} RDD',
+    commandMsg : '+tip  {AMOUNT} RDD',
+
+    hookTipOpen : function(){
+        var currentUser = $(".profile-card-inner").attr("data-screen-name");
+        if(currentUser){
+            $("#reddTipUser").val("@" + currentUser);
+        }
+    },
+
+    tipClicked : function(){
+        var button = $(this),
+            isMessage = button.closest('.tweet-button').find('.tweet-btn').find('.messaging-text').is(':visible'),
+            textArea = button.closest('form').find('.tweet-box'),
+            showUser = false,
+            requireUser = false;
+
+        if(!isMessage) {
+            showUser = true;
+            requireUser = true;
+        }
+
+        RDD.modal.open(function(tipAmount, tipUser){
+            var text = textArea.text()
+
+            if(isMessage) {
+                text += RDD.helpers.getCommand(RDD.site.commandMsg, tipAmount, tipUser)
+            }
+            else {
+                text += RDD.helpers.getCommand(RDD.site.command, tipAmount, tipUser);
+            }
+
+            textArea.text( text );
+        }, showUser, requireUser);
+    },
+
+    addButtons : function(){
+        // Add buttons to tweets
+        $('.tweet-button').append(RDD.site.buttonHtml);
+        $('.tip-btn').css('height', '37px');
+
+        // Now fix the height of the buttons and fix the onclick function (add tip)
+        //twitterFixButtons ();
+
+
+        $("body").on("click", ".tip-btn", RDD.site.tipClicked);
+    },
+
+    initialize: function(){
+        RDD.site.addButtons();
+    }
+};
+
+RDD.sites.twitch = {
+
+    buttonHtml : '<button class="button primary_button tip" style="padding: 1.5px;width: 22%;">Tip</button>',
+    command    : '@tipreddcoin +tip {RECIPIENT} {AMOUNT} RDD',
+
+    tipClicked : function(){
+        var textArea = $(this).closest('.chat-interface').find('textarea');
+
+        RDD.modal.open(function(tipAmount, tipUser){
+            var text = textArea.val()
+
+                text += RDD.helpers.getCommand(RDD.site.command, tipAmount, tipUser);
+
+            textArea.val( text );
+        }, true, false);
+    },
+
+    addButtons : function(){
+        // Add a button to the Twitch chat
+        $('.send-chat-button').append(RDD.site.buttonHtml);
+        $('.send-chat-button button:first').css("width", "75%");
+
+        $("body").on("click", ".tip", RDD.site.tipClicked);
+    },
+
+    checkWidth : function (waitTime)
+    {
+        // Don't start the script until the page has been loaded correctly
+        if ($('.send-chat-button button:first').width() > 120)
+        {
+            RDD.site.addButtons();
+        }
+        else if (waitTime <= 4096)
+        {
+            setTimeout(function(){
+                RDD.site.checkWidth(waitTime * 2);
+            }, waitTime);
+        }
+    },
+
+    initialize: function(){
+        if ($('.chat-option-buttons').length != 0)
+        {
+            RDD.site.checkWidth(16);
+        }
+    }
+};
+
+dbg("we made it this far...");
+
+$(function(){
+
+    dbg("actually in the document ready function?!?!");
+    var site = RDD.helpers.getCurrentSite();
+
+    //early exit. The site wasn't resolved correctly.
+    if(site === false) {
+        dbg("site not found");
+        return;
+    }
+
+    dbg("setting site...");
+    //set the current site
+    RDD.site = RDD.sites[site];
+
+    dbg("initializing modal");
+    //initialiaze the modal dialogue
+    RDD.modal.initialize();
+
+    dbg("initializing site");
+    //initialize the current site.
+    RDD.site.initialize();
+
+});
---
+++ source/lib/main.js
@@ -0,0 +1,41 @@
+var pageMod = require("sdk/page-mod");
+var self = require("sdk/self");
+var Request = require("sdk/request").Request;
+
+console.log("starting...");
+
+var startContentScript = function(popupHtml){
+
+    console.log("popup url requested");
+    console.log(popupHtml);
+
+    pageMod.PageMod({
+        //for whatever reason, the array of includes didn't seem to be working. Ultra-escaped cryptic regex to the rescue.
+        include: /https?:\/\/(?:www\.)?(?:reddit.com\/r\/.*\/comments\/|twitter\.com|twitch\.tv).*/,
+
+        contentScriptFile: [
+            self.data.url("jquery.js"),
+            self.data.url("content.js")
+        ],
+
+        contentStyleFile: self.data.url("styles.css"),
+
+        contentScriptOptions: {
+            baseUrl : self.data.url(""),
+            popupHtml: popupHtml
+        }
+    });
+
+}
+
+var popup = Request({
+    url: self.data.url("popup.html"),
+//    onComplete: startContentScript
+    onComplete: function(data){
+
+        startContentScript(data.text)
+    }
+});
+
+popup.get();
+
